ca32998fd329b1e671e704f29aa30966931379e7
ling-cy <lingcy.work@gmail.com>
2023-05-04 11:35:53 -0400

    2 files modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import * as React from 'react';
-import './style.css';
+import Button from '@mui/material/Button';
 
-export default function App() {
+type AppProps = {
+  url?: string;
+  color?: React.CSSProperties;
+  text?: string;
+};
+
+const App: React.FC<AppProps> = ({
+  url = 'https://www.google.com.hk/',
+  color = '#FF0068',
+  text = 'Login',
+}) => {
+  const handleOnClick = () => {
+    window.open(url, '_blank');
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Button
+      variant="contained"
+      onClick={handleOnClick}
+      disableRipple
+      sx={{
+        backgroundColor: color,
+        minWidth: 150,
+        minHeight: 40,
+        borderRadius: 20,
+      }}
+    >
+      {text}
+    </Button>
   );
-}
+};
+
+export default App;
