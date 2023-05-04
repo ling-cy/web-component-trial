9a93bfa3d98ecc6d10a6a0d755dd3ab5ac715a51
ling-cy <lingcy.work@gmail.com>
2023-05-04 12:43:25 -0400

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import reactToWebComponent from 'react-to-webcomponent';
+import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const LoginButton = reactToWebComponent(App, React, ReactDOM);
+customElements.define('login-button', LoginButton);
